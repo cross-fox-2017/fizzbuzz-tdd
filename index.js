283a8e7ba1bb3e7dab994a747c44b6338b0aa6b9
yoma sofwan <email": null>
2017-02-13 11:58:18 +0700

    jika selain fizz buzz dan fizzbuzz

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function(num){
-  let data = [[5,'buzz'],[3,'fizz'],[15,'fizzbuzz']]
-  let result = ""
+  var data = [[5,'buzz'],[3,'fizz'],[15,'fizzbuzz']]
+  var result = num
   data.forEach(function(x){
     if(num%x[0]==0){
       result = x[1]
--- test/index.js
+++ test/index.js
@@ -6,9 +6,11 @@ const fizzbuzz = require('../index');
 describe('menjalankan fungsi fizzbuzz', function() {
   it('menghasilkan fizz', function() {
       fizzbuzz(18).should.equal("fizz");
+      fizzbuzz(7).should.equal(7);
   });
   it('menghasilkan buzz', function() {
       fizzbuzz(35).should.equal("buzz");
+      fizzbuzz(34).should.equal(34);
   });
   it('menghasilkan fizzbuzz', function() {
       fizzbuzz(30).should.equal("fizzbuzz");
