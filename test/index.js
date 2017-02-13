ea92d0557fff3b09200141c91d76a1180c23c39b
yoma sofwan <email": null>
2017-02-13 11:16:34 +0700

    selesai, belum refact

--- test/index.js
+++ test/index.js
@@ -1,16 +1,16 @@
 const assert    = require('assert')
 const chai      = require('chai')
 const should    = chai.should()
-
+const fizzbuzz = require('../index');
 
 describe('menjalankan fungsi fizzbuzz', function() {
   it('menghasilkan fizz', function() {
-      math.fizzbuzz(3).should.equal("fizz");
+      fizzbuzz(3).should.equal("fizz");
   });
   it('menghasilkan buzz', function() {
-      math.fizzbuzz(5).should.equal("buzz");
+      fizzbuzz(5).should.equal("buzz");
   });
   it('menghasilkan fizzbuzz', function() {
-      math.fizzbuzz(15).should.equal("fizzbuzz");
+      fizzbuzz(15).should.equal("fizzbuzz");
   });
 });
