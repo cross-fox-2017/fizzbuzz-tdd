158f9f619b5cba64a02c54674596cc8c4e4c3610
Raditya <arya.raditya26@gmail.com>
2017-02-13 12:29:50 +0700

    tmabah titik

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,9 @@
 var test = {
   fizzbuzz: function(input) {
     if    (input % 3 == 0 && input % 5 == 0) return "FizzBuzz"
-    else if  (input % 3 == 0 ) return "Fizz"
-    else if  (input % 5 == 0 ) return "Buzz"
-    else return input
+    else if  (input % 3 == 0 ) return "Fizz";
+    else if  (input % 5 == 0 ) return "Buzz";
+    else return input;
 
   }
 }
