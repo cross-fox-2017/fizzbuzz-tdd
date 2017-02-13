9166fe5ad45754791975e22a2f89adb4027ba20a
Syanmil <syanmil@yahoo.com>
2017-02-13 11:50:27 +0700

    testing created and passed

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,23 @@
-function fizzbuzz() {
-  return 
+let fizz = {
+  buzz: function (number) {
+    if(number%3 == 0 && number%5 == 0){
+      return 'FizzBuzz'
+    }
+    if(number%3 == 0){
+      return 'Fizz'
+    }
+    if(number%5 == 0){
+      return 'Buzz'
+    }
+    return number
+  },
+  buzzer : function(number){
+    result = []
+    for (let i = 1; i <= number; i++){
+      result.push(fizz.buzz(i))
+    }
+    return result
+  }
 }
-module.exports = fizzbuzz
+
+module.exports = fizz
--- test/test.js
+++ test/test.js
@@ -1,20 +1,36 @@
 const expect = require('chai').expect
-const fizzbuzz = require('../fizzbuzz')
+const fizz = require('../fizzbuzz.js')
+console.log(fizz.buzz);
 
 describe("fizzbuzz testing", function() {
-  it('expect to to return something', function() {
-    expect(fizzbuzz).to.be.a('function')
+  it('expect fizzbuzz to be function', function() {
+    expect(fizz.buzz).to.be.a('Function')
   })
-  it('expect to return Fizz when modular 3', function() {
-    expect(fizzbuzz(3)).to.be.equal('Fizz')
+  it('expect fizzbuzz result in Number if not fizzbuzzed', function() {
+    expect(fizz.buzz(1)).to.be.a('Number')
   })
-  it('expect to return Buzz when modular 5', function() {
-    expect(fizzbuzz(5)).to.be.equal('Buzz')
+  it('expect fizzbuzz to return Fizz when modular 3', function() {
+    expect(fizz.buzz(3)).to.be.equal('Fizz')
   })
-  it('expect to return FizzBuzz when modular 3 AND modular 5', function() {
-    expect(fizzbuzz(15)).to.be.equal('FizzBuzz')
+  it('expect fizzbuzz 99 to return Fizz ', function() {
+    expect(fizz.buzz(99)).to.be.equal('Fizz')
   })
-  it('expect to return 4', function() {
-    expect(fizzbuzz(4)).to.be.equal(4)
+  it('expect fizzbuzz to return Buzz when modular 5', function() {
+    expect(fizz.buzz(5)).to.be.equal('Buzz')
+  })
+  it('expect fizzbuzz 50 to return Buzz', function() {
+    expect(fizz.buzz(50)).to.be.equal('Buzz')
+  })
+  it('expect fizzbuzz to return FizzBuzz when modular 3 AND modular 5', function() {
+    expect(fizz.buzz(15)).to.be.equal('FizzBuzz')
+  })
+  it('expect fizzbuzz 90 to return FizzBuzz', function() {
+    expect(fizz.buzz(90)).to.be.equal('FizzBuzz')
+  })
+  it('expect fizzbuzz to return 4', function() {
+    expect(fizz.buzz(4)).to.be.equal(4)
+  })
+  it('expect fizzbuzzer to be an array', function() {
+    expect(fizz.buzzer(4)).to.be.a('Array')
   })
 })
