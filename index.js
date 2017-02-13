0ebb56fc494f25f4bb009ea7110b5cff21f26d85
yoma sofwan <email": null>
2017-02-13 11:27:23 +0700

    perbaikan inden

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = function(num){
-  const data = [[5,'buzz'],[3,'fizz'],[15,'fizzbuzz']]
-  let hasil = ""
+  let data = [[5,'buzz'],[3,'fizz'],[15,'fizzbuzz']]
+  let result = ""
   data.forEach(function(x){
     if(num%x[0]==0){
-     hasil = x[1]
+      result = x[1]
     }
   })
   return hasil
