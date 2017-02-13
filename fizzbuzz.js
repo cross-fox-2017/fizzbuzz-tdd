bdc38c99dbd7146fe0478c4c93b4c6b2ce7db346
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-13 11:36:15 +0700

    Complete Fizz Buzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,16 @@
 
 var Testing = {
   fizzbuzz: function(input) {
-    var arr = []
-    for (var i = 1; i <= 100; i++) {
-      arr.push(i)
-    }
-    for (var i = 0; i < arr.length; i++) {
-      if(input%3==0 && input%5==0){
-        return "FIZZ BUZZ"
-      }else if(input%3==0){
-        return "FIZZ"
-      }else if(input%5==0){
-        return "BUZZ"
-      }else{
-        return input
-      }
-    }
+    // var arr = []
+    // for (var i = 1; i <= 100; i++) {
+    //   arr.push(i)
+    // }
+    // for (var i = 0; i < arr.length; i++) {
+      if(input%3==0 && input%5==0) return "FIZZ BUZZ"
+      else if(input%3==0) return "FIZZ"
+      else if(input%5==0) return "BUZZ"
+      else return input
+    // }
 
   }
 }
